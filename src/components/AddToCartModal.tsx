ace5c6efba6572dce3692955a2e4f2a168587c0b
Armando Arlan Joergensen <armando-a-j@hotmail.com>
2021-01-08 22:21:44 -0300

    --Fixx
    Lazy load components

---
+++ src/components/AddToCartModal.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddToCartModal: React.FC = () => {
+  return <div><p>iudhiaushdiuashdiuahduis</p></div>
+}
+
+export default AddToCartModal;
---
+++ src/pages/catalog/products/[slug].tsx
@@ -0,0 +1,37 @@
+import { Router, useRouter } from "next/router";
+import dynamic from 'next/dynamic';
+import { useState } from "react";
+
+
+const AddToCartModal = dynamic(
+  ()=> import('../../../components/AddToCartModal'),
+  {loading:  
+    () => <p>Carregando...</p>,
+    // ssr: false faz com que o componente nunca seja renderizado pelo servidor 
+    // ele aguardar para renderizar no lado do cliente, usamos quando precisa alguma
+    //variavel global do Brwoser por exemplo
+    
+  }
+)
+
+export default function Product(){
+  const router = useRouter();
+  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
+
+  function handleToCart(){
+    setIsAddToCartModalVisible(true);
+  }
+  return(
+    <div>
+      <h1>{router.query.slug}</h1>
+
+      <button   onClick={handleToCart}>
+        Add to cart
+      </button>
+
+      {isAddToCartModalVisible &&
+        <AddToCartModal />
+      }
+    </div>
+  )
+}
